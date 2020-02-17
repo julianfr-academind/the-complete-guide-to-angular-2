ef1a5587a4b2d2769e86915c9186653c7392655d
Julián <jfernandezreyes@anac.gob.ar>
2020-02-17 11:27:21 -0300

    Assignment 1: Practicing Components

---
+++ assignment-01/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ assignment-01/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  template: `<p>Succes message</p>`,
+  styles: [`p{ background: green;}`]
+})
+export class SuccessAlertComponent {
+
+}
